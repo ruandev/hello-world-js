29f1cfe653030cadb64f8641212ea83b42b3c3c5
Ruan Victor <ruan.techno@gmail.com>
2024-03-23 21:09:16 -0300

    Aprendendo outras operações matemáticas

--- operacoes-matematicas.js
+++ operacoes-matematicas.js
@@ -1,4 +1,5 @@
 // Soma
+console.log("Soma")
 console.log(2 + 2)
 const numero1 = 1
 const outroNumero = 5
@@ -6,3 +7,20 @@ const maisUmNumero = 6
 
 const resultado = numero1 + outroNumero + maisUmNumero
 console.log(resultado)
+
+// Subtração
+console.log("Subtração")
+console.log(5 - 3)
+let primeiroNumero = 2
+let segundoNumero = 3
+
+let subtracao = primeiroNumero - segundoNumero
+console.log(subtracao)
+
+// Multiplicação
+console.log("Multiplicação")
+console.log(2 * 3)
+
+// Divisão
+console.log("Divisão")
+console.log(10/2)
