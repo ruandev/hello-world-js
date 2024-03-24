57437ef258d005a9e8bdf12e6c7f02e9c178fb81
Ruan Victor <ruan.techno@gmail.com>
2024-03-23 22:08:55 -0300

    aprendendo sobre laços de repetição

---
+++ repeticao.js
@@ -0,0 +1,13 @@
+/** 
+for (let i = 0; i < 10; i++)  {
+    console.log(i)
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`O valor de i é ${i}`)
+}
+*/
+
+for (let i = 10; i >= 0; i--) {
+    console.log(`O valor de i é ${i}`)
+}
