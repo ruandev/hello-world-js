57f057c682cadb991fb25a85d7e5273c68e8e2c7
Ruan Victor <ruan.techno@gmail.com>
2024-03-23 21:27:19 -0300

    Aprendendo sobre strings

---
+++ strings.js
@@ -0,0 +1,12 @@
+let nome = "Ruan Victor"
+
+console.log(nome.split(" "))
+
+let tamanhoDaString = nome.length
+console.log(tamanhoDaString)
+
+console.log(nome.charAt(5))
+console.log(nome.charAt(0))
+
+console.log(nome.toUpperCase())
+console.log(nome.toLowerCase())
