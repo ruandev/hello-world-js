6a5f2559c514b2379951895f3f06728a8bad17ce
Ruan Victor <ruan.techno@gmail.com>
2024-03-23 22:12:41 -0300

    Aprendendo sobre while

--- repeticao.js
+++ repeticao.js
@@ -6,8 +6,16 @@ for (let i = 0; i < 10; i++)  {
 for (let i = 1; i <= 10; i++) {
     console.log(`O valor de i é ${i}`)
 }
-*/
+
 
 for (let i = 10; i >= 0; i--) {
     console.log(`O valor de i é ${i}`)
+}
+*/
+
+let numero = 10
+while (numero > 0) {
+    console.log(`O valor de numero é ${numero}`)
+
+    numero = numero - 1
 }
