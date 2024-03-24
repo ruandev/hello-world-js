77782d2960ff41df63d563d10a0a5d58c53b3fbe
Ruan Victor <ruan.techno@gmail.com>
2024-03-23 21:00:07 -0300

    Aprendendo sobre variaveis

---
+++ variaveis.js
@@ -0,0 +1,41 @@
+// Nomes de variáveis válidos
+
+let nomeDaVariavel
+let nome12345
+let nome_nome$
+let NOME
+let nome
+let Nome
+let nOme
+
+/** 
+Nome inválidos de variáveis
+
+let 1nome
+let nome@
+let let
+*/
+
+let minhaPrimeiraVariavel
+
+console.log(minhaPrimeiraVariavel)
+
+minhaPrimeiraVariavel = "Ruan"
+
+console.log(minhaPrimeiraVariavel)
+
+minhaPrimeiraVariavel = "Victor"
+
+console.log(minhaPrimeiraVariavel)
+
+minhaPrimeiraVariavel = 29
+
+console.log(minhaPrimeiraVariavel)
+
+let meuTime = "Bahia"
+
+console.log(meuTime)
+
+var nomeDoEstado = "São Paulo"
+
+console.log(nomeDoEstado)
